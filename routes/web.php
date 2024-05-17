02e590af5361de3f9eb8bb4336b6579c19ea05f9
Chaimaa <houas.chaimaa@gmail.com>
2024-05-17 11:43:45 +0100

    view

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landing');
 });
+Route::get('/login', function () {
+    return view('Auth.login');
+});
+Route::get('/register', function () {
+    return view('Auth.register');
+});
 
