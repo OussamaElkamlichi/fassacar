8bc0b6f4c50c5b9e06ffbc483f64294fb47ac483
Elgadisalma <elgadi.salma0@gmail.com>
2024-05-17 10:18:16 +0100

    Login & register routes & layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Auth.login');
 });
+
+
+Route::get('/register', [AuthController::class, 'registerView'])->name('registerView');
