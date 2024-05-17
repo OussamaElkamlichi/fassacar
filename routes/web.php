ff2311d17981511ac36c066ef72778031fd19a61
Elgadisalma <elgadi.salma0@gmail.com>
2024-05-17 12:00:22 +0100

    page 404 && add footer to layout

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\clientViewsController;
 
-// Route::get('/', function () {
-//     return view('landing');
-// });
+Route::get('/404', function () {
+    return view('404');
+});
 
 
 Route::get('/', [clientViewsController::class,'showHome'])->name('home.show');
